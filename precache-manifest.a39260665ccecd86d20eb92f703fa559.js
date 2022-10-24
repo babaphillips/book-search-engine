6bfa7246295808ee415f72db1562ea711ec63037
babaphillips <amandavphillips@icloud.com>
2022-10-24 17:54:16 -0400

    Updates

---
+++ precache-manifest.a39260665ccecd86d20eb92f703fa559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebeae2a7554ab6e5878ecfe49f7a2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ed1d61f13c5f64f4326",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fc14a041939c809513c7",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "4ed1d61f13c5f64f4326",
+    "url": "/static/js/2.d44511ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d44511ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc14a041939c809513c7",
+    "url": "/static/js/main.81e0d537.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
